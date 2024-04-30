07cec2775c5faeb0e45cfced182cba8e7616a886
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-04-30 14:21:36 -0700

    added useEffect

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,7 @@
+//import PropTypes library
+import PropTypes from "prop-types"
+
+//MovieCard function component
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -8,4 +12,23 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       {movie.title}
     </div>
   );
+};
+
+
+//This defines all props constraints 
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string,
+    director: PropTypes.shape({
+      name: PropTypes.string,
+      bio: PropTypes.string
+    }),
+    description: PropTypes.string.isRequired,
+    genre: PropTypes.shape({
+      name: PropTypes.string,
+      description: PropTypes.string
+    }),
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };
