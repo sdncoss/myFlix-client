dd659f61a9fcdcb3bcfdba7bae53bef544664cf9
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-05-06 13:53:45 -0700

    Added Bootstrap Form

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -36,28 +36,28 @@ export const LoginView = ({ onLoggedIn }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control
           type="text"
           placeholder="username"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
           required
         />
-      </label>
-      <label>
-        Password:
-        <input
+      </Form.Group>
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control
           type="password"
           placeholder="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
         />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
+      </Form.Group>
+      <Button variant="primary" type="submit">Submit</Button>
+    </Form>
   );
 };
