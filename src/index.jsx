623056243b88dc625b7123d0342c234c8ff631fd
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-05-16 11:25:06 -0700

    edited import

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { MainView } from "../src/components/main-view/main-view";
 import React from "react";
 //Importing Bootstrap styles
-import { Container } from 'react-bootstrap/Container';
+import { Container } from 'react-bootstrap';
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
