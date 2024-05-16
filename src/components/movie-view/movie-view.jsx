42b4e286f61a556841661d9384be1f9cc0e2ccc7
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-05-16 11:22:50 -0700

    removed no longer existing file

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
-import "./movie-view.scss";
+
 
 
 export const MovieView = ({ movie }) => {
